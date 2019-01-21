4f4a1090d1f0db19cd1547a4704f63e72bdbbbf6
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-21 16:01:47 -0500

    Added docs to twich service

--- src/app/services/twitch.service.ts
+++ src/app/services/twitch.service.ts
@@ -3,16 +3,36 @@ import { TwitchUser } from '../models/twitch-user';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+/**
+ * Twitch service to interact with twitch data
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class TwitchService {
 
+  // Location of back-end service
   private BASE_URL: String = 'https://twitch-web-integrator.herokuapp.com/';
 
+  /**
+   * Create our service with http client
+   * @param _http HttpClient to perform restful calls with
+   */
   constructor(private _http: HttpClient) { }
 
+  /**
+   * Get the twitch user
+   * @param channelName Channel name that we are trying to get the user for
+   */
   public getTwitchUser(channelName: String): Observable<TwitchUser> {
     return this._http.get<TwitchUser>(this.BASE_URL + 'user?channelName=' + channelName);
+  }
+
+  /**
+   * Get the twitch followers
+   * @param channelName  Channel name that we are trying to get the followers for
+   */
+  public getTwitchFollowers(channelName: String): Observable<TwitchUser> {
+    return this._http.get<TwitchUser>(this.BASE_URL + 'followers?channelName=' + channelName);
   }
 }
