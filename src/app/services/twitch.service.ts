93fc1b11cbfabcb5c99725e989f2294d8599d81a
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-24 00:38:03 -0500

    Added url to environment variables
    This allows me to twitch from testing the remote and the local based off
    where I build.

--- src/app/services/twitch.service.ts
+++ src/app/services/twitch.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { TwitchFollower } from '../models/twitch-follower';
 import { StreamerConstants } from '../constants/streamer-constants';
+import { environment } from '../../environments/environment';
 
 /**
  * Twitch service to interact with twitch data
@@ -12,9 +13,6 @@ import { StreamerConstants } from '../constants/streamer-constants';
   providedIn: 'root'
 })
 export class TwitchService {
-
-  // Location of back-end service
-  private BASE_URL: String = 'https://twitch-web-integrator.herokuapp.com/';
 
   /**
    * Create our service with http client
@@ -26,20 +24,26 @@ export class TwitchService {
    * Get the twitch user
    */
   public getUser(): Observable<TwitchUser> {
-    return this._http.get<TwitchUser>(this.BASE_URL + 'user?channelName=' + StreamerConstants.CHANNEL_NAME);
+    return this._http.get<TwitchUser>(
+      environment.twitchWebIntegratorUrl + 'user?channelName=' + StreamerConstants.CHANNEL_NAME
+      );
   }
 
   /**
    * Get the twitch followers
    */
   public getRecentFollowers(): Observable<TwitchFollower[]> {
-    return this._http.get<TwitchFollower[]>(this.BASE_URL + 'recent/followers?channelName=' + StreamerConstants.CHANNEL_NAME);
+    return this._http.get<TwitchFollower[]>(
+      environment.twitchWebIntegratorUrl + 'recent/followers?channelName=' + StreamerConstants.CHANNEL_NAME
+      );
   }
 
   /**
    * Get the twitch follower total
    */
   public getTotalFollowers(): Observable<number> {
-    return this._http.get<number>(this.BASE_URL + 'total/followers?channelName=' + StreamerConstants.CHANNEL_NAME);
+    return this._http.get<number>(
+      environment.twitchWebIntegratorUrl + 'total/followers?channelName=' + StreamerConstants.CHANNEL_NAME
+    );
   }
 }
