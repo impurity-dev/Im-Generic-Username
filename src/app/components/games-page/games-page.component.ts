a880f7875f1013c8d66ca0eaad4cc0bc482ac35b
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-26 11:12:02 -0500

    Made gameslist a gridview instead
    This way, I can spruce it up a bit with images

--- src/app/components/games-page/games-page.component.ts
+++ src/app/components/games-page/games-page.component.ts
@@ -6,42 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./games-page.component.css']
 })
 export class GamesPageComponent {
-  public games: string[] = [
-    'AOE II',
-    'Among The Sleep',
-    'Astroneer',
-    'Battlerite',
-    'Bioshock 1 & 2',
-    'Borderlands 2 & Pre-Sequel',
-    'Chivalry Medieval Warfare',
-    'Counter-Strike Global Offensive',
-    'Dark Souls 3',
-    'DayZ',
-    'Dead By Daylight',
-    'Deceit',
-    'Dolphin Emulator: Paper Mario, LOZ Windwaker',
-    'The Elder Scrolls V Skyrim',
-    'Fallout 3, New Vegas, & 4',
-    'The Forest',
-    'FTL Faster Than Light',
-    'Golf It',
-    'GTA V',
-    'Human Fall Flat',
-    'Killing Floor 2',
-    'Left For Dead 2',
-    'Natural Selection 2',
-    'No Man\'s Sky',
-    'PAYDAY 2',
-    'PUBG',
-    'Q.U.B.E',
-    'Rampage Knights',
-    'Resident Evil 5',
-    'Rocket League',
-    'SCP: Secret Laboratory',
-    'Sid Meier\'s Civilization V',
-    'Slap City',
-    'Starbound',
-    'Subnautica',
-    'Surgeon Simulator: Experience Reality'
+  public games: string[][] = [
+    [
+      'AOE II',
+      'Among The Sleep',
+      'Astroneer',
+      'Battlerite',
+      'Bioshock 1 & 2',
+      'Borderlands 2 & Pre-Sequel'
+    ],
+    [
+      'Chivalry Medieval Warfare',
+      'Counter-Strike Global Offensive',
+      'Dark Souls 3',
+      'DayZ',
+      'Dead By Daylight',
+      'Deceit'
+    ],
+    [
+      'Dolphin Emulator: Paper Mario, LOZ Windwaker',
+      'The Elder Scrolls V Skyrim',
+      'Fallout 3, New Vegas, & 4',
+      'The Forest',
+      'FTL Faster Than Light',
+      'Golf It'
+    ],
+    [
+      'GTA V',
+      'Human Fall Flat',
+      'Killing Floor 2',
+      'Left For Dead 2',
+      'Natural Selection 2',
+      'No Man\'s Sky'
+    ],
+    [
+      'PAYDAY 2',
+      'PUBG',
+      'Q.U.B.E',
+      'Rampage Knights',
+      'Resident Evil 5',
+      'Rocket League'
+    ],
+    [
+      'SCP: Secret Laboratory',
+      'Sid Meier\'s Civilization V',
+      'Slap City',
+      'Starbound',
+      'Subnautica',
+      'Surgeon Simulator: Experience Reality'
+    ]
   ];
 }
