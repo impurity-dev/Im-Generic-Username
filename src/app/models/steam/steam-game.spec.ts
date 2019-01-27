054f567f37bb543c24620a9da1bd146630df09c1
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-27 11:43:44 -0500

    Update services
    The services now have the updated restful API for its calls.

---
+++ src/app/models/steam/steam-game.spec.ts
@@ -0,0 +1,7 @@
+import { SteamGame } from './steam-game';
+
+describe('SteamGame', () => {
+  it('should create an instance', () => {
+    expect(new SteamGame()).toBeTruthy();
+  });
+});
---
+++ src/app/models/steam/steam-game.ts
@@ -0,0 +1,8 @@
+export class SteamGame {
+  public appid: number;
+  public name: string;
+  public playtimeForver: number;
+  public imgIconUrl: string;
+  public imgLogoUrl: string;
+  public hasCommunityVisibleStats: boolean;
+}
